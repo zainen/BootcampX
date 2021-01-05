0a9a472e06f92ff1e7737f5d1baa3b70c2d66b8c
zainen <zainensuzuki@gmail.com>
2021-01-05 21:05:24 +0000

    + teachers/assist-req

--- 3_queries_group-by/speedy_student.sql
+++ 3_queries_group-by/speedy_student.sql
@@ -8,5 +8,5 @@ JOIN assignments ON assignments.id = assignment_id
 WHERE students.end_date IS NULL
 GROUP BY student
 HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
-ORDER BY average_assignment_duration asc
+ORDER BY average_assignment_duration asc;
     
