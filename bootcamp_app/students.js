ec949e6ea2c1faf855621e604fc0015f81957d33
zainen <zainensuzuki@gmail.com>
2021-01-07 03:53:37 +0000

    altered with promises

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,13 +1,16 @@
 const { Pool } = require('pg');
-const month = process.argv[2]
-const maxResults = process.argv[3]
+const month = process.argv[2];
+// const maxResults = parseInt(process.argv[3]) ? parseInt(process.argv[3]) : 5
+const maxResults = process.argv[3] || 5
+
+const values = [`%${month}%`, maxResults];
 
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
   host: 'localhost',
   database: 'bootcampx'
-})
+});
 
 // pool.query(`
 // SELECT students.id as student_id, students.name as name, cohorts.name as cohort
@@ -26,17 +29,22 @@ pool.query(`
   SELECT students.id as student_id, students.name as name, cohorts.name as cohort
   FROM students
   JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name LIKE '${month}%'
-  LIMIT ${maxResults || 5};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`)
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+`, values)
+.then(students => {
+  return new Promise ((resolve, reject) => {
+    students.rows.forEach((user) => {
+      if (!user) {
+        return reject('no user');
+      }
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    });
+    return resolve(students);
   })
-})
-.catch(err => {
-  console.log(err)
-})
+}).catch(err => {
+  return err;
+});
 
 // pool.query(`
 // SELECT students.id, students.name, cohorts.name as cohort
