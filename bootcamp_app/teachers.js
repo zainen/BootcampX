45b5088cf86be903e735e49997f6f9876b1bbb58
zainen <zainensuzuki@gmail.com>
2021-01-07 02:23:26 +0000

    node-pg js queries

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,40 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+})
+
+pool.query(`SELECT 
+teachers.name AS teacher,
+cohorts.name as cohort,
+count(*) as total_assistances
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
+GROUP BY teachers.name, cohorts.name
+ORDER BY total_assistances desc;`)
+.then(res => {
+  res.rows.forEach(teacher => {
+    console.log(`${teacher.cohort}: ${teacher.teacher}`)
+  })
+})
+
+// pool.query(`
+// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+// FROM teachers
+// JOIN assistance_requests ON teacher_id = teachers.id
+// JOIN students ON student_id = students.id
+// JOIN cohorts ON cohort_id = cohorts.id
+// WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+// ORDER BY teacher;
+// `)
+// .then(res => {
+//   res.rows.forEach(row => {
+//     console.log(`${row.cohort}: ${row.teacher}`);
+//   })
+// });
